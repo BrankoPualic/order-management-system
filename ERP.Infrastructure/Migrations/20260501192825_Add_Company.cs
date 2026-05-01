83ca83f2295944f245ccbbf075b5ae54cb10f0d1
Branko Pualić-Radujko <pualicbranko@gmail.com>
2026-05-01 21:35:50 +0200

    feat: add company migration

---
+++ ERP.Infrastructure/Migrations/20260501192825_Add_Company.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ERP.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ERP.Infrastructure.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20260501192825_Add_Company")]
+    /// <inheritdoc />
+    public partial class Add_Company : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PublicId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Notes = table.Column<string>(type: "text", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Address_City = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Address_Country = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Address_State = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Address_Street = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Address_ZipCode = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                    table.UniqueConstraint("AK_Companies_PublicId", x => x.PublicId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
