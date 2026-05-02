6375f0cef46daad4785a8af34a711b493ebabc32
Branko Pualić-Radujko <pualicbranko@gmail.com>
2026-05-02 14:48:54 +0200

    feat: add company rename, change description, change address, delete endpoints

--- ERP.API/Endpoints/CompanyEndpoints.cs
+++ ERP.API/Endpoints/CompanyEndpoints.cs
@@ -14,10 +14,17 @@ public static class CompanyEndpoints
         group.MapGet("/", GetCompanies);
         group.MapGet("/{id}", GetCompany);
         group.MapPost("/", RegisterCompany);
+        group.MapPatch("/{id}/name", RenameCompany);
+        group.MapPatch("/{id}/description", ChangeDescription);
+        group.MapPatch("/{id}/address", ChangeAddress);
+        group.MapDelete("/{id}", DeleteCompany);
     }
 
     private record RegisterCompanyRequest(string Name, string Description, AddressRequest Address);
     private record AddressRequest(string Street, string City, string State, string Country, string ZipCode);
+    private record RenameCompanyRequest(string Name);
+    private record ChangeDescriptionRequest(string Description);
+    private record ChangeAddressRequest(AddressRequest Address);
 
     private static async Task<ICompanyQueries.CompanyResponse[]> GetCompanies(ICompanyQueries queries, CancellationToken cancellationToken = default) =>
         await queries.GetCompaniesAsync(cancellationToken);
@@ -45,5 +52,57 @@ public static class CompanyEndpoints
         await unitOfWork.SaveChangesAsync(cancellationToken);
 
         return Results.Created($"/companies/{company.PublicId}", company);
+    }
+
+    private static async Task<IResult> RenameCompany(Guid id, RenameCompanyRequest request, IUnitOfWork unitOfWork, CancellationToken cancellationToken = default)
+    {
+        var company = await unitOfWork.GetRepository<Company, Company.CompanyId>().TryFindAsync(id, cancellationToken);
+
+        if (company == null) return Results.NotFound();
+
+        company.Rename(request.Name);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Results.NoContent();
+    }
+
+    private static async Task<IResult> ChangeDescription(Guid id, ChangeDescriptionRequest request, IUnitOfWork unitOfWork, CancellationToken cancellationToken = default)
+    {
+        var company = await unitOfWork.GetRepository<Company, Company.CompanyId>().TryFindAsync(id, cancellationToken);
+
+        if (company == null) return Results.NotFound();
+
+        company.ChangeDescription(request.Description);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Results.NoContent();
+    }
+
+    private static async Task<IResult> ChangeAddress(Guid id, ChangeAddressRequest request, IUnitOfWork unitOfWork, CancellationToken cancellationToken = default)
+    {
+        var company = await unitOfWork.GetRepository<Company, Company.CompanyId>().TryFindAsync(id, cancellationToken);
+
+        if (company == null) return Results.NotFound();
+
+        company.ChangeAddress(new(
+            request.Address.Street,
+            request.Address.City,
+            request.Address.State,
+            request.Address.Country,
+            request.Address.ZipCode
+        ));
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Results.NoContent();
+    }
+
+    private static async Task<IResult> DeleteCompany(Guid id, IUnitOfWork unitOfWork, CancellationToken cancellationToken = default)
+    {
+        await unitOfWork.GetRepository<Company, Company.CompanyId>().Delete(id, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return Results.NoContent();
     }
 }
